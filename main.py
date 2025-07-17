e42dbc2291365f6c4ac7149d0c5271bd2ce19400
Lukas Scheucher <scheuclu@gmail.com>
2025-07-17 19:51:13 +0100

    WIP

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ from panels.response_codes import get_reponse_codes_table
 import streamlit as st
 from datetime import datetime, timedelta
 
-st.title('Cyber dashboard')
+st.title("Cyber dashboard")
 
-min_datetime, max_datetime =st.date_input(label='Select date range', value=[datetime.today()-timedelta(days=1), datetime.today()])
+min_datetime, max_datetime = st.date_input(
+    label="Select date range",
+    value=[datetime.today() - timedelta(days=1), datetime.today()],
+)
 
 df = asyncio.run(get_reponse_codes_table(min_datetime, max_datetime))
 
--- panels/response_codes.py
+++ panels/response_codes.py
@@ -1,6 +1,6 @@
-
 import polars as pl
 from logfire.query_client import AsyncLogfireQueryClient
+
 
 async def get_reponse_codes_table(min_datetime, max_datetime):
     query = """
@@ -20,16 +20,18 @@ ORDER BY
   num_seen DESC;
     """
 
-    async with AsyncLogfireQueryClient(read_token='pylf_v1_eu_tRNktBRvGfzSbJnQ5Z5rywXPfbR18bTP9kLxLKZ2F2pC') as client:
-
-        df_from_arrow = pl.from_arrow(await client.query_arrow(sql=query,
-                                    min_timestamp=min_datetime,
-                                    max_timestamp=max_datetime))
+    async with AsyncLogfireQueryClient(
+        read_token="pylf_v1_eu_tRNktBRvGfzSbJnQ5Z5rywXPfbR18bTP9kLxLKZ2F2pC"
+    ) as client:
+        df_from_arrow = pl.from_arrow(
+            await client.query_arrow(
+                sql=query, min_timestamp=min_datetime, max_timestamp=max_datetime
+            )
+        )
         return df_from_arrow
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import asyncio
 
     asyncio.run(get_reponse_codes_table())
